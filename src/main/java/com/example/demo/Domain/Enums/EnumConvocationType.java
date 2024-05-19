84008ac1ece27d7dea449f9220f00d8102d562e8
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-05-19 18:38:44 -0500

    chore: add enums convocationType, condition, stateRequest

--- src/main/java/com/example/demo/Domain/Enums/EnumConvocationType.java
+++ src/main/java/com/example/demo/Domain/Enums/EnumConvocationType.java
@@ -1,4 +1,19 @@
 package com.example.demo.Domain.Enums;
 
 public enum EnumConvocationType {
+    MONITORING(1),
+    GRANT(2),
+    RESIDENCE(3);
+    private int value;
+    EnumConvocationType(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/example/demo/Domain/Enums/EnumStateRequest.java
+++ src/main/java/com/example/demo/Domain/Enums/EnumStateRequest.java
@@ -1,4 +1,20 @@
 package com.example.demo.Domain.Enums;
 
 public enum EnumStateRequest {
+    PENDING(1),
+    ACTIVE(2),
+    REJECT(3),
+    ACCEPT(4);
+    private int value;
+    EnumStateRequest(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
