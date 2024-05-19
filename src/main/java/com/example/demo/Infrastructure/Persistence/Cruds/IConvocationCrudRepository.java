fa127f5cbbfaed74b81a212538e4a006c624dd0e
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-05-18 19:59:06 -0500

    build: Database configuration

--- src/main/java/com/example/demo/Infrastructure/Persistence/Cruds/IConvocationCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Infrastructure.Persistence.Cruds;
-
-import com.example.demo.Domain.Entities.Convocation;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IConvocationCrudRepository extends CrudRepository<Convocation, Integer> {
-}
--- src/main/java/com/example/demo/Infrastructure/Persistence/Repositories/ConvocationRepository.java
+++ src/main/java/com/example/demo/Infrastructure/Persistence/Repositories/ConvocationRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.Infrastructure.Persistence.Repositories;
 
 import com.example.demo.Domain.Interfaces.IConvocationRepository;
-import com.example.demo.Infrastructure.Persistence.Cruds.IConvocationCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ConvocationRepository implements IConvocationRepository {
-    @Autowired
-    private IConvocationCrudRepository convocationCrudRepository;
+
 }
