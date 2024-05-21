77d61c7e88d58e127d600a29ee7afcc83fe5ea08
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-05-21 17:20:45 -0500

    feat: Create the enums for convocationtype and conditionconvocation

--- src/main/java/com/example/demo/Application/IConvocationService.java
+++ src/main/java/com/example/demo/Application/IConvocationService.java
@@ -1,9 +1,11 @@
 package com.example.demo.Application;
 
+import com.example.demo.Domain.Entities.Convocation;
 import com.example.demo.Domain.Entities.ConvocationType;
 
 import java.util.List;
 
 public interface IConvocationService {
+   List<Convocation> getAllConvocation();
    List<ConvocationType> getAllConvocationTypes();
 }
--- src/main/java/com/example/demo/Domain/Interfaces/IConvocationRepository.java
+++ src/main/java/com/example/demo/Domain/Interfaces/IConvocationRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.Domain.Interfaces;
 
+import com.example.demo.Domain.Entities.Convocation;
+
+import java.util.List;
+
 public interface IConvocationRepository {
+   List<Convocation> getAllConvocation();
 }
